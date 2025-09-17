0c5662aac095e3cf557ae438a7170d992e7e0023
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-17 16:42:10 +0530

    updated

---
+++ circuitbreakerapi/src/main/java/com/bofa/circuitbreakerapi/CircuitbreakerapiApplication.java
@@ -0,0 +1,13 @@
+package com.bofa.circuitbreakerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitbreakerapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircuitbreakerapiApplication.class, args);
+    }
+
+}
