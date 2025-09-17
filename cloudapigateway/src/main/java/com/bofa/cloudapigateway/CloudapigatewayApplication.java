0ec62512b9128fa07d81eefe4da7fe0691c4be71
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-09-17 14:33:31 +0530

    updated

---
+++ cloudapigateway/src/main/java/com/bofa/cloudapigateway/CloudapigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bofa.cloudapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudapigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudapigatewayApplication.class, args);
+    }
+
+}
---
+++ cloudapigateway/src/test/java/com/bofa/cloudapigateway/CloudapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bofa.cloudapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudapigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
